7b7d8ffa24505121c129e32df245e790e88be3fa
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2022-11-05 23:52:33 -0400

    Passing Tests

--- tests.cpp
+++ tests.cpp
@@ -8,34 +8,43 @@
 Coord3D pointAa = {20,30,40};
 Coord3D pointAb = {15,21,76};
 Coord3D pointAc = {83,23,56};
-
-TEST_CASE("TASK A"){
-    CHECK(length(&pointAa) == 53.8516);
-    CHECK(length(&pointAb) == 80.2627);
-    CHECK(length(&pointAc) == 102.7327);
-}
-
 Coord3D pointBa = {62,63,16};
 Coord3D pointBb = {41,64,71};
 Coord3D pointBc = {36,88,41};
 Coord3D pointBd = {26,45,22};
 
+TEST_CASE("TASK A"){
+    CHECK(length(&pointAa) == 53.85164807134504);
+    CHECK(length(&pointAb) == 80.26207074328447);
+    CHECK(length(&pointAc) == 102.73266277090261423241013344731515453545515079035184951605249632);
+    CHECK(length(&pointBa) == 89.827612681179499328082381256795598116868114937551075322934613989);
+    CHECK(length(&pointBb) == 104.0096149401583);
+    CHECK(length(&pointBc) == 103.54226190305097);
+}
+
 TEST_CASE("TASK B"){
     CHECK(fartherFromOrigin(&pointBa, &pointBb) == &pointBb);
     CHECK(fartherFromOrigin(&pointBc, &pointBd) == &pointBc);
-    CHECK(fartherFromOrigin(&pointBb, &pointBc) == &pointBc);
+    CHECK(fartherFromOrigin(&pointBb, &pointBc) == &pointBb);
     CHECK(fartherFromOrigin(&pointBa, &pointBd) == &pointBa);
 }
 
-//Task C? void function?
+// Coord3D pos = {0,0,100.0};
+// Coord3D vel = {1,5,0.2};
 
-Coord3D *taskEa = &pointAa;
-Coord3D *taskEb = &pointAb;
-Coord3D *taskEc = &pointAc;
-Coord3D *taskEd = &pointBa;
+// //Task C? void function?
+// TEST_CASE("TASK C"){
+//     CHECK(move(&pos, &vel, 12) == void);
+// }
 
-TEST_CASE("TASK E"){
-    CHECK(createCoord3D(20,30,40) == taskEa);
-    CHECK(createCoord3D(15,21,76) == taskEb);
-    CHECK(createCoord3D(62,63,16) == taskEc);
-}
+
+// Coord3D *taskEa = &pointAa;
+// Coord3D *taskEb = &pointAb;
+// Coord3D *taskEc = &pointAc;
+// Coord3D *taskEd = &pointBa;
+
+// TEST_CASE("TASK E"){     //creates a new memory address within the function 
+//     CHECK(createCoord3D(20,30,40) == );
+//     CHECK(createCoord3D(15,21,76) == );
+//     CHECK(createCoord3D(62,63,16) == );
+// }
